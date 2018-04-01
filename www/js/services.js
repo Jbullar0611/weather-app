6478ce3910088547e9d5823c2828d4ba0af4e723
jbullar0611 <jaspreetbhullar502@gmail.com>
2018-04-01 01:11:38 -0400

    Initial Build

--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,60 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('CityData', function() {
   // Might use a resource here that returns a JSON array
+var data = {
+  'Kitchener':{
+    currentTemp:6,
+    description:'Light Rain Showers',
+    yesterday: -3,
+    tomorrow: 4 
+  },
+  'Waterloo':{
+    currentTemp:12,
+    description:'Hot and humid',
+    yesterday: -8,
+    tomorrow: 9 
+  },
+  'Toronto':{
+    currentTemp:-7,
+    description:'Cloudy',
+    yesterday: 6,
+    tomorrow: -10 
+  },
+  'Brampton':{
+    currentTemp:-8,
+    description:'Colder with wind',
+    yesterday: 2,
+    tomorrow: -3 
+  },
+  'Vancouver':{
+    currentTemp:5,
+    description:'Partly Cloudy',
+    yesterday: 3,
+    tomorrow: 11
+  }
+};
+return {
+  getData:function(city){
+    return data[city];
+  }
+}
+ });
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
+factory('AlertData', function() {
+  // Might use a resource here that returns a JSON array
+var data = {
+  'Kitchener':{
+    currentTemp:6,
+    description:'Light Rain Showers',
+    yesterday: -3,
+    tomorrow: 4 
+  }
+};
+return {
+  getData:function(city){
+    return data[city];
+  }
+}
 });
